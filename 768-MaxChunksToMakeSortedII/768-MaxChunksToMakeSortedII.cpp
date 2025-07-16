d3030db017567c0f9629f78a16d4e832b2b21732
Ayush Sahu <119402957+Ashu-x@users.noreply.github.com>
2025-07-17 02:46:53 +0530

    Create 768-MaxChunksToMakeSortedII

---
+++ 768-MaxChunksToMakeSortedII/768-MaxChunksToMakeSortedII.cpp
@@ -0,0 +1,22 @@
+// Last updated: 7/17/2025, 2:46:52 AM
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int n = arr.size();
+        vector<int>pre(n);
+        vector<int>suf(n);
+        pre[0] = arr[0];
+        suf[n-1] = arr[n-1];
+        for(int i=1;i<arr.size();i++){
+            pre[i] = max(pre[i-1],arr[i]);
+        }
+        for(int i=n-2;i>=0;i--){
+            suf[i] = min(suf[i+1],arr[i]);
+        }
+        int count = 0;
+        for(int i=0;i<n-1;i++){
+            if(pre[i]<=suf[i+1]) count++;
+        }
+        return count+1;
+    }
+};
