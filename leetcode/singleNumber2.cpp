9712c77e56591412b0763c538449c321e09ac314
Ashu-x <beingayush66@gmail.com>
2025-07-25 00:09:39 +0530

    single number2 bucketMethod

--- leetcode/singleNumber2.cpp
+++ leetcode/singleNumber2.cpp
@@ -10,3 +10,13 @@ int singleNumber(vector<int>& nums) {
 
     return ans;
 }
+
+// USING BUCKETS ONES AND TWOS
+int singleNumber(vector<int>& nums) {
+int ones=0,twos=0;
+        for(int i:nums){
+            ones = ((ones^i) & (~twos)) ;
+            twos = ((twos^i) &(~ones));
+        }
+  return ones;
+}
