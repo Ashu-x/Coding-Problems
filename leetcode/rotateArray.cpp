b65615df075e4e46fab1103bd441e542110ba0d5
Ashu-x <beingayush66@gmail.com>
2025-08-28 00:51:37 +0530

    GCD approach

--- leetcode/rotateArray.cpp
+++ leetcode/rotateArray.cpp
@@ -13,3 +13,21 @@ void rotate(vector<int>& nums, int k) {
         helper(nums,0,k-1);
         helper(nums,k,n-1);
 }
+
+// GCD METHOD
+void rotate(vector<int>& nums, int k){
+    int n = nums.size();
+    k%= n;
+    int gcd = __gcd(n,k);
+    for(int i=0; i<gcd; i++){
+        int left = i;
+        int currEle = nums[left];
+        do{
+            int nextIdx = (currEle+d)%n ;
+            int nextEle = nums[nextIdx];
+
+            nums[nextIdx] = currEle;
+            left = nextIdx;
+        } while(left  != i);
+    }
+}
