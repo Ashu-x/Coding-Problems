6aa389b02304c6472ed6dfaa1afbe2c9d7ea5ed6
Ashu-x <beingayush66@gmail.com>
2025-09-26 01:02:51 +0530

    Single Root to Leaf Path

---
+++ leetcode/Trees/RootToLeafPath.cpp
@@ -0,0 +1,18 @@
+vector<int> rootToleafPath(TreeNode* root,int target){
+    if(root==NULL) return {};
+    vector<int> ans;
+    array(root,ans, target);
+    return ans; 
+}
+
+bool array(TreeNode* root, vector<int>& ans){
+    if(root==NULL) false;
+
+    ans.push_back(root->val);
+    if(target == root->val) return true;
+    array((root->left, ans, target) || array(root->right, ans, target)){
+        return true;
+    }
+    ans.pop_back();
+    return false;
+}
