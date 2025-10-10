72f129f13e866e8920aead24de24fb68c5b6b377
Ashu-x <beingayush66@gmail.com>
2025-10-11 00:26:08 +0530

    Both approaches constant space

--- leetcode/Trees/flattenTreeToLL.cpp
+++ leetcode/Trees/flattenTreeToLL.cpp
@@ -1,14 +1,32 @@
+//This aproach follows reverse PostORder traversal => Right Left Root
+TreeNode* prev = nullptr;
+void flat(TreeNode* node) {
+    if (node == nullptr) return;
+
+    flat(node->right);
+    flat(node->left);
+
+    node->right = prev;
+    node->left = nullptr;
+    prev = node;    //Here we are marking prev to node whose reverse postorder is completed
+}
+
 void flatten(TreeNode* root) {
-        while(root != NULL){
-            if(root->left != NULL){
-                TreeNode* prev = root->left;
-                while(prev->right!= NULL){
-                    prev = prev->right;
-                }
-                prev->right = root->right;
-                root->right = root->left;
-                root->left = NULL;
-            }
-                root = root->right;
+    flat(root);
+}
+
+//Below is approach where we are preordering and connecting each right node to its root->right
+void flatten(TreeNode* root){
+    TreeNode* curr = root;
+    while(curr != NULL){
+
+        if(curr->left != NULL){
+        TreeNode* prev = curr->left;
+        while(prev->right) prev = prev->right;
+        
+        prev->right = curr->right;
+        curr->right = curr->left;
         }
+        curr = curr->right;
+    }
 }
