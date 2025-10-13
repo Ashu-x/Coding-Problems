0f7e93f604e6573b63dabbeef95ef203d8500e35
Ashu-x <beingayush66@gmail.com>
2025-10-13 23:43:34 +0530

    LCA in BST

---
+++ leetcode/Trees/LCAinBST.cpp
@@ -0,0 +1,22 @@
+TreeNode* lcafind(TreeNode* root, TreeNode* p, TreeNode* q){
+    while(root != NULL){
+        if(root->val < p->val && root->val < q->val) root = root->right;
+
+        if(root->val > p->val && root->val > q->val) root = root->left;
+
+        else return root;
+    }
+}
+
+//Recursion approach
+TreeNode* lcafind(TreeNode* root, TreeNode* p, TreeNode* q){
+    if(root == NULL) return NULL;
+    int curr = root->val;
+    if(curr < q->val && curr < p->val){
+        lcafind(root->right, p,q);
+    }
+    if(curr > q->val && curr > p->val){
+        lcafind(root->left, p,q);
+    }
+    return root;
+}
