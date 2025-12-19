0248f5c51638b7079e4fd1ac1f96b6ff576a9140
Ayush Sahu <119402957+Ashu-x@users.noreply.github.com>
2025-12-20 01:46:44 +0530

    IDID by stack & problem details
    Added explanation and example for the pattern problem.

--- leetcode/IDIDwithstack.cpp
+++ leetcode/IDIDwithstack.cpp
@@ -13,3 +13,13 @@ string smallestNumber(string pattern) {
     }
     return ans;
 }
+
+You are given a O-indexed string pattern of length n consisting of the characters 'I' meaning increasing and 'D' meaning decreasing.
+Ip: pattern = "IIIDIDDD"
+Op: "123549876"
+Explanation:
+At indices 0, 1, 2, and 4 we must have that num[i] < num[i+1].
+At indices 3, 5, 6, and 7 we must have that num[i] > num[i+1].
+Some possible values of num are "245639871", "135749862", and "123849765".
+It can be proven that "123549876" is the smallest possible num that meets the conditions.
+Note that "123414321" is not possible because the digit '1' is used more than once.
