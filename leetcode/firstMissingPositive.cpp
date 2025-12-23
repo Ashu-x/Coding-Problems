acf356d0d4c48412fa38e2b298a7a752483d15fd
Ayush Sahu <119402957+Ashu-x@users.noreply.github.com>
2025-12-24 00:08:24 +0530

    fistMissinNum approach added
    Refactor code to improve readability and maintainability.

--- leetcode/firstMissingPositive.cpp
+++ leetcode/firstMissingPositive.cpp
@@ -3,15 +3,17 @@ int firstMissingPositive(vector<int>& nums) {
         for(int i=0; i<n; i++){
             if(nums[i] < 0) nums[i] = 0;
         }
-        for(int i=0; i<n; i++){
-            int ele = abs(nums[i]);
+        for(int i=0; i<n; i++){         // making places -ve to indicate ele of that place exist in array
+            int ele = abs(nums[i]); 
             if(ele > 0 && ele <=n){
-                if(nums[ele-1] == 0) nums[ele -1] = (-1)*(n+1) ;
-                else if(nums[ele-1] > 0) nums[ele-1] *= -1 ;
+                if(nums[ele-1] == 0) nums[ele -1] = (-1)*(n+1) ; 
+                else if(nums[ele-1] > 0) nums[ele-1] *= -1 ;        // if some ele is +ve mark its place -ve
             }
         }
         for(int i=1; i<=n; i++){
             if(nums[i-1] >= 0) return i;
+                //if we found ele > n if wouldn't be -ve ise so w returing i(that place)
         }
         return n+1;
 }
+//FINDING SMALLEST POSITIVE INT not exist in unsorted array
